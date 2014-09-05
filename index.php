cebb517053d735ee88b4015fac4b4a6e9929dd09
Jevon van Dijk <jevon@atabix.nl>
2014-09-05 09:59:10 +0200

    Remove components folder
    Signed-off-by: Jevon van Dijk <jevon@atabix.nl>

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
-namespace LinkLinker;
+<html>
+	<head>
 
-echo "LinkLinker";
 
+		<link rel="stylesheet" href='/static/css/style.css' />
+	</head>
+	<body>
+		
+		<h1>Test</h1>	
+
+		<main class="ui three column grid">
+		  <aside class="column">1</aside>
+		  <section class="column">2</section>
+		  <section class="column">3</section>
+		</main>
+	</body>
+</html>
--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -4,7 +4,9 @@ module.exports = function(grunt) {
 
     sass: {
       options: {
-        includePaths: ['components/foundation/scss']
+        includePaths: ['components/foundation/scss'],
+        includePaths: ['components/semantic-ui'],
+        includePaths: ['components/semantic-ui-sass']
       },
       dist: {
         options: {
